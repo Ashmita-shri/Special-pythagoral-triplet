9e6d1eb9918ece4594b49b75f0a196ba8f605bd2
Ashmita <Ashmita.shrivastava@csgi.com>
2023-07-05 15:26:34 +0530

    updated code

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+
+def pythagoreanTriplet(n):
+    result=[]
+    for a in range(1, n):
+        denom = 2*(n-a)
+        num = 2*a**2 + n**2 - 2*n*a
+        if denom > 0 and num % denom == 0:
+          c = num // denom
+          b = n - a - c
+          if b > a:
+            result.append((a*b*c))
+    if len(result)>0:
+        print(max(result))
+    else:
+        print(-1)
